e268c9c5760591c90fe5fc4b8b6266fd5e34aa83
Chris <christianto.g.14@gmail.com>
2019-04-09 10:35:55 +0700

    add testing skeleton

---
+++ tests/lib/ApiSimulator.php
@@ -0,0 +1,36 @@
+<?php
+namespace Tests\Lib;
+
+class ApiSimulator implements \LINE\Interfaces\ApiInterface {
+    public function getChannelID():string{
+        
+    }
+
+    public function setChannelID(string $id):void{
+        
+    }
+
+    public function getChannelSecret():string{
+        
+    }
+
+    public function setChannelSecret(string $id):void{
+        
+    }
+
+    public function getToken():\LINE\Token{
+        
+    }
+
+    public function setToken(\LINE\Token $token):void{
+        
+    }
+
+    public function get($url, $param, $options, $auth_type){
+        
+    }
+    
+    public function post($url, $param, $options, $auth_type){
+        
+    }
+}
